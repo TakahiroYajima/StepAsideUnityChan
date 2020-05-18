a3e2a83938d9777b0b43856358b596ce408ada07
TakahiroYajima <yajimatakahiro@outlook.jp>
2020-05-18 21:54:43 +0900

    Lesson6課題提出
    通常課題及び発展課題の作成

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : SingletonMonoBehaviour<Emitter> {
+
+    private List<EmitObject> emitList = new List<EmitObject>();
+    private List<GameObject> instanceList = new List<GameObject>();
+
+    [SerializeField] private GameObject playerObj = null;
+    [SerializeField] private GameObject cameraObj = null;
+    
+    [SerializeField] private float emmitInstanceDistance = 20f;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //生成
+		if(emitList.Count > 0)
+        {
+            if(emitList[0].instancePos.z - playerObj.transform.position.z <= emmitInstanceDistance)
+            {
+                GameObject instanceObj = Instantiate(emitList[0].instanceObj) as GameObject;
+                instanceObj.transform.position = emitList[0].instancePos;
+                instanceObj.transform.SetParent(this.transform);
+                instanceList.Add(instanceObj);
+                emitList.RemoveAt(0);
+            }
+        }
+        //削除
+        if(instanceList.Count > 0)
+        {
+            if (instanceList[0] == null)
+            {
+                instanceList.RemoveAt(0);
+            }
+            else
+            {
+                if (instanceList[0].transform.position.z < cameraObj.transform.position.z)
+                {
+                    Destroy(instanceList[0].gameObject);
+                    instanceList.RemoveAt(0);
+                }
+            }
+        }
+	}
+
+    public void AddEmmitPrefab(EmitObject emitObj)
+    {
+        emitList.Add(emitObj);
+    }
+}
+
+/// <summary>
+/// 障害・コインオブジェクト生成用データ
+/// </summary>
+public class EmitObject
+{
+    public GameObject instanceObj = null;
+    public Vector3 instancePos = Vector3.zero;
+}
---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+    public GameObject carPrefab;
+
+    public GameObject coinPrefab;
+
+    public GameObject conePrefab;
+
+    private int startPos = -160;
+    private int goalPos = 120;
+    private float posRange = 3.4f;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = startPos; i < goalPos; i+= 15)
+        {
+            int num = Random.Range(1, 11);
+            if(num <= 2)
+            {
+                for(float j = -1; j <= 1; j+= 0.4f)
+                {
+                    EmitObject emit = new EmitObject();
+                    emit.instanceObj = conePrefab;
+                    emit.instancePos = new Vector3(4 * j, conePrefab.transform.position.y, i);
+                    Emitter.Instance.AddEmmitPrefab(emit);
+                }
+            }
+            else
+            {
+                for(int j = -1; j <= 1; j++)
+                {
+                    int item = Random.Range(1, 11);
+                    int offsetZ = Random.Range(-5, 6);
+                    if(1 <= item && item <= 6)
+                    {
+                        EmitObject emit = new EmitObject();
+                        emit.instanceObj = coinPrefab;
+                        emit.instancePos = new Vector3(posRange * j, coinPrefab.transform.position.y, i + offsetZ);
+                        Emitter.Instance.AddEmmitPrefab(emit);
+                    }
+                    else if(7 <= item && item <= 9)
+                    {
+                        EmitObject emit = new EmitObject();
+                        emit.instanceObj = carPrefab;
+                        emit.instancePos = new Vector3(posRange * j, carPrefab.transform.position.y, i + offsetZ);
+                        Emitter.Instance.AddEmmitPrefab(emit);
+                    }
+                }
+            }
+        }
+	}
+}
